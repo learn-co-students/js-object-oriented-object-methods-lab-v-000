3c645462aa97d533528efb2e67b98296bc1b4a77
gb23 <gregbenj@hotmail.com>
2017-10-19 19:53:46 -0700

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function BoardMember(name, homeState, training) {
     this.veto = function(){return "No, I must disagree"};
     this.approve = function(){return "You can do that!"};
     this.doCharity = function(){return "I like to help people."};
-    this.releasePressStatement = function(){return "You will see great things from Scuber."};
-    this.sayHi = function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`};
+    this.releasePressStatement = function(){
+        return "You will see great things from Scuber."
+    };
+    this.sayHi = function(){
+        return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    };
 }
 
