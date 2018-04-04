b0c427145757b5930258b29912f2af1d66b1aa69
Hoon Lee <yl.hoony@gmail.com>
2018-04-04 02:37:47 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,6 @@
      return "You will see great things from Scuber.";
    }
    this.sayHi = () => {
-     return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
+     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
  }
