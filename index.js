3ee69679d91c90e599107d518c655b5624123c49
blinded93 <blinded93@gmail.com>
2018-04-15 19:52:08 -0700

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function BoardMember (name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
+  
   this.veto = function () {
     return "No, I must disagree"
   };
