6b7f56cc72dff968000220f7b7629bffb3d0fd3d
catalanowebdevelopment <catalano.work@gmail.com>
2018-05-01 11:28:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function BoardMember (name, homestate, training) {
+function BoardMember (name, homeState, training) {
     this.name = name
-    this.homestate = homestate
+    this.homeState = homeState
     this.training = training
     
     this.veto = function() {
@@ -20,6 +20,6 @@ function BoardMember (name, homestate, training) {
     }
     
     this.sayHi = function() {
-        return(`Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`)
+        return(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)
     }
 }
