7f5bf173618e6221eaa5964e3432cd314cd47883
catalanowebdevelopment <catalano.work@gmail.com>
2018-05-01 11:26:01 -0400

    Done.

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ function BoardMember (name, homestate, training) {
     this.training = training
     
     this.veto = function() {
-        console.log('No, I must disagree')
+        return('No, I must disagree')
     }
     
     this.approve = function() {
-        console.log('You can do that!')
+        return('You can do that!')
     }
     
     this.doCharity = function() {
-        console.log('I like to help people.')
+        return('I like to help people.')
     }
     
     this.releasePressStatement = function() {
-        console.log('You will see great things from Scuber.')
+        return('You will see great things from Scuber.')
     }
     
     this.sayHi = function() {
-        console.log(`Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`)
+        return(`Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`)
     }
 }
