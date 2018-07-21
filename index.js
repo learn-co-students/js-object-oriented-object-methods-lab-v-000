91d79dfd564f7ec9a67e353a6252fb8ef96931ef
Rygel-XVI <micha232@umn.edu>
2018-07-20 17:29:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 
-function BoardMember(name, homestate, training) {
+function BoardMember(name, homeState, training) {
   this.name = name;
-  this.homestate = homestate;
+  this.homeState = homeState;
   this.training = training;
-  console.log(this.name, this.homestate, this.training)
-
   this.veto = () => { return `No, I must disagree` };
-  console.log(this.veto())
   this.approve = () => { return `You can do that!` };
-  console.log(this.approve())
-
   this.doCharity = () => { return `I like to help people.` };
-  console.log(this.doCharity())
-
   this.releasePressStatement = () => { return `You will see great things from Scuber.` };
-  console.log(this.releasePressStatement())
-
-  this.sayHi = () => { return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.` };
-  console.log(this.sayHi())
+  this.sayHi = () => { return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.` };
 }
