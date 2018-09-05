0dc0d44c6c4c2105f398f6d3873685cc44aaf1f6
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 22:36:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// BoardMember that sets the following properties: name, homeState, and training.
-// Add following methods to the class:
-// veto — returns No, I must disagree
-// approve — returns You can do that!
-// doCharity — returns I like to help people.
-// releasePressStatement — returns You will see great things from Scuber.
-// sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
-
 function BoardMember (name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
