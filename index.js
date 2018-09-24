62939124347a41424adf9a927c9a0970a595118a
NeedsAUsername <kevinchi118@gmail.com>
2018-09-23 22:48:04 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function BoardMember(name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  this.veto = function() {
+    return 'No, I must disagree';
+  }
+  this.approve = () => 'You can do that!';
+
+  this.doCharity = () => 'I like to help people.';
+
+  this.releasePressStatement = () => 'You will see great things from Scuber.';
+
+  this.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+ }
