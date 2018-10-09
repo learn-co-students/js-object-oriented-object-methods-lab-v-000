5d23ed083b4362873eae78b99e5d5777bdebc947
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 03:09:25 +0000

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function BoardMember(name, homeState, training) {
     return "You will see great things from Scuber."};
     
  this.sayHi = function(){
-   return "Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}."
+   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
  };
 }
