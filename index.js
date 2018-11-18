c62081a97b83468592e0f70fa80d6905e544abb3
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 21:15:27 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function BoardMember (name, homeState, training) {
     }
     
     this.sayHi = function(){
-     return "Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}."
+    return "Hi, my name is " + this.name + ". I am from " + this.homeState + ", and I was trained in " + this.training + "."
     }
 }
 
