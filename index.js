a36dc658bdc920b656e488dd3ef96c953fbf822c
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-12 17:38:52 +0000

    Done.

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ function BoardMember(name, homeState, training) {
   this.approve = function() { return "You can do that!";}
   this.doCharity = function() { return "I like to help people.";}
   this.releasePressStatement = function() {return "You will see great things from Scuber.";}
-  this.sayHi = function(){ return "Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.";}
+  this.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
 }
