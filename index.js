16e54d123552eb76d855bde6d5f5c30a6f69a5d9
Christopher P. Dundon <chris.dundon@gmail.com>
2018-12-12 15:02:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function User (name, email){
-  this.name = name;
-  this.email = email;
-  this.sayHello = function(){
-    console.log("Hello everybody!");
-  }
-}
-
 function BoardMember (name, homeState, training) {
 	this.name = name;
 	this.homeState = homeState;
