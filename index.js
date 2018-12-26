e29891b9251bc1f97aa8c40eaf9683468358e882
charlie  lee <ctlee9096@gmail.com>
2018-12-26 03:24:11 +0000

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ this.name = name;
 this.homeState = homeState;
 this.training = training;
 this.veto = function() {
-console.log(`No, I must disagree`);
+ return 'No, I must disagree'
 }
 this.approve = function() {
-console.log(`You can do that!`);
+ return 'You can do that!'
 }
 this.doCharity = function () {
-console.log(`I like to help people.`);
+ return 'I like to help people.'
+}
+this.releasePressStatement = function () {
+  return 'You will see great things from Scuber.'
+}
+this.sayHi = function  () {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 }
