5e9f15ff82814047cb4e410c6fe9e11fec7e6173
corytveten <corytveten@gmail.com>
2019-04-19 18:44:40 -0700

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,5 @@ function BoardMember (name, homeState, training) {
     this.releasePressStatement = function() {
         return "You will see great things from Scuber.";
     }
-    this.sayHi = function() {
-        return  `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-    }
+
 }
