c717bff949006802976a862deda415fb97239290
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-28 15:10:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-function BoardMember (name,homeState,training) {
+function BoardMember(name, home_state, training) {
   this.name = name
-  this.homeState = homeState
+  this.homeState = home_state
   this.training = training
-
-  this.veto = function () {
-     return "No, I must disagree"
-    }
-
-  this.approve = function () {
-    return "You can do that!"
-  }
-
-  this.doCharity = function () {
-    return "I like to help people."
-  }
-
-  this.releasePressStatement = function () {
-     return "You will see great things from Scuber."
-  }
-
-  this.sayHi = function () {
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
+  this.veto = function veto() { return "No, I must disagree" }
+  this.approve = function approve () { return "You can do that!" }
+  this.doCharity = function charity () { return "I like to help people."}
+  this.releasePressStatement = function () { return  "You will see great things from Scuber."}
+  this.sayHi = function () { return `Hi, my name is ${name}. I am from ${home_state}, and I was trained in ${training}.`}
 }
