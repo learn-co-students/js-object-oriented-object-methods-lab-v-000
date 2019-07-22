b70bed756a8c3bc2289c0cd31c8d780b9c44e3ab
Heshie Brody <4hbrody@gmail.com>
2019-07-21 23:52:11 -0400

    Done.

--- index.js
+++ index.js
@@ -23,10 +23,3 @@
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
    };
  };
-
-// Add following methods to the class:
-// veto — returns No, I must disagree
-// approve — returns You can do that!
-// doCharity — returns I like to help people.
-// releasePressStatement — returns You will see great things from Scuber.
-// sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
